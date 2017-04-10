e568ec7bf36368ec7812137a54478d8d5c2467e9
cyab <2455991405@qq.com>
2017-04-10 19:29:36 +0800

    开始提交了

---
+++ src/com/mypackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mypackage;
+
+/**
+ * Created by Administrator on 2017-04-10.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
